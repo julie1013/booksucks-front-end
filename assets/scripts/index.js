1d300b402ac887582ef13ce0d9ba1e6ba14903d3
julie1013 <julie1380@verizon.net>
2016-10-03 15:56:57 -0400

    Fixed "add to to read list" buttons
    Said buttons weren't being properly targeted. That's fixed now.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,12 +7,12 @@ $(document).ready(function () {
   $('#sign-in').on('submit', events.onSignIn);
   $('#sign-out').on('submit', events.onSignOut);
   $('#change-password').on('submit', events.onChangePassword);
-  $('li').on('submit', 'form', events.onAddToMyToReadList);
   $('#to-read-list').on('submit', 'form', events.onRemoveBookFromMyToReadList);
   $('#show-my-to-read-list').hide();
   $('#sign-out').hide();
   $('#change-password').hide();
   events.onShowMasterList();
+  $('.master-list ol').on('submit', 'li form', events.onAddToMyToReadList);
   $('#master-book-list').hide();
   $('#to-read-list').hide();
 });
