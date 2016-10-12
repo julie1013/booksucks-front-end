b20d0e6f2b277415c7e3e81dc0a59e9acb2916d4
julie1013 <julie1380@verizon.net>
2016-10-11 23:45:48 -0400

    Trying to fix the problem where the addNote isn't firing properly.
    AddNote button isn't firing properly. Trying to fix it.

--- assets/scripts/ui-showToReadList.js
+++ assets/scripts/ui-showToReadList.js
@@ -14,33 +14,33 @@ const onShowMyToReadListSuccess = function (data){
     }
     qualifiedBookID = data[i].id;
     //qualifiedBookId is assigned the id of a qualified_books entry
-    $('#toReadList').append('<li data-id=' + qualifiedBookID + '>' +
+    $('#toReadList').append('<li id=' + qualifiedBookID + '>' +
     //append list items to to-read list
     '<span class=title>' + data[i].book.title +'</span>' +
     //each list item has a title...
-    '<span class=author>' +  data[i].book.author + '</span>'+ '<p data-id=noteSpace' + i +
-     'class="note-space-here">Notes:</p>' + data[i].notes +
+    '<span class=author>' +  data[i].book.author + '</span>'+ '<p id=noteSpace' + i +
+     ' class="note-space-here">Notes:</p>' + data[i].notes +
     //...and an author... and a space where you can see a note...
-    '<form data-id=removeFromMyToReadList' + i + ' class=remove-from-my-to-read-list>' +
+    '<form id=removeFromMyToReadList' + i + ' class=remove-from-my-to-read-list>' +
     //...and each note is given a form button to enable removal
-    '<input type=submit value="Remove Book">' + '</form>' +
+    '<input type=Submit value="Remove Book">' + '</form>' +
     //...and each book on the list has a removal option...
-    '<form data-id=addNote' + i + ' class="add-note">' + '<input type=submit value="Write a note?">' +
+    '<form id=addNote' + i + ' class="add-note">' + '<input type=Submit value="Write a note?">' +
     //...and this is the field in which you can type your note; field button included
     '<input name="notes"' + 'input type="text"'+ 'value="">' + '</form>' +
     //...the rest of the notes form
-    '<form data-id=removeNote' + i + ' class="remove-notes">' + '<input type=submit value="Remove Note">' +
+    '<form id=removeNote' + i + ' class="remove-notes">' + '<input type=Submit value="Remove Note">' +
     //...option to remove note
     '</form>' + '</li>');
     //...list item closed
     if (data[i].notes === "Your note shows here!"){
       $('#removeNote' + i).hide();
       $('#addNote' + i).show();
-      $('#addNote').on('submit', events.onAddNote);
+      $('#addNote' + i).on('click', events.onAddNote);
     } else {
       $('#removeNote' + i).show();
       $('#addNote' + i).hide();
-      $('#removeNote' + i).on('click', events.onRemoveNote);
+      $('#removeNote' + i).on('submit', events.onRemoveNote);
     }
   }
 };
